b65ca7776d39d0d2bdf1b88d316fe1b0785f83d3
ayomahansani <ayohansi17@gmail.com>
2025-01-24 11:35:08 +0530

    create flower slice

---
+++ src/reducers/FlowerSlice.ts
@@ -0,0 +1,26 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {Flower} from "../models/flower.ts";
+
+const initialState: Flower[] = [];
+
+const flowerSlice = createSlice({
+    name: "flower",
+    initialState,
+    reducers: {
+        addFlower: (state, action) => {
+            state.push(action.payload);
+        },
+        updateFlower: (state, action) => {
+            const index = state.findIndex((flower) => flower.flower_code === action.payload.flower_code);
+            if (index !== -1) {
+                state[index] = action.payload;
+            }
+        },
+        deleteFlower: (state, action) => {
+            return state.filter((flower) => flower.flower_code !== action.payload.flower_code);
+        },
+    },
+});
+
+export const {addFlower, updateFlower, deleteFlower} = flowerSlice.actions;
+export default flowerSlice.reducer;
